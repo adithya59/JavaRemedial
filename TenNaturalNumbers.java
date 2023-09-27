f73cedfa75cd71841e29f5b3597ccd7140adceec
adithya59 <145443401+adithya59@users.noreply.github.com>
2023-09-27 02:31:38 -0700

    Add files via upload

---
+++ TenNaturalNumbers.java
@@ -0,0 +1,15 @@
+/***********************************************************************************************
+ * Name				:TenNaturalNumbers.java
+ * Description		:Java program to display the first 10 natural numbers
+ * Author			:Adithya P Binu
+ * Version			:1.0
+ * Date				:27/09/2023
+ * *********************************************************************************************/
+public class TenNaturalNumbers {
+	public static void main(String[] args) {
+		System.out.println("The First 10 natural numbers are:");
+		for (int i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+}
