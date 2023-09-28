21f31e08b60ab988f50f60da2423f3e18706a949
adithya59 <145443401+adithya59@users.noreply.github.com>
2023-09-28 16:03:03 +0530

    Update SumofnNaturalOddNumbers.java

--- SumofnNaturalOddNumbers.java
+++ SumofnNaturalOddNumbers.java
@@ -8,12 +8,13 @@
 import java.util.Scanner;
 public class SumofnNaturalOddNumbers {
 	public static void main(String[] args) {
-		int limit,res=0;
+		int limit,res=0,j=1;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number:");
 		limit = sc.nextInt();
-		for(int i=1;i<=limit;i+=2) {
-			res=res+i;
+		for(int i=0;i<limit;i++) {
+			res=res+j;
+			j+=2;
 		}
 		System.out.println("Sum of "+limit+" natural odd numbers:"+res);
 	}
